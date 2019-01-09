d5d13b12d4c9525ea50d82f0b6d96fe1826ec902
Sam Spilsbury <smspillaz@gmail.com>
2019-01-09 08:23:52 +0200

    stylometry: Don't import everything in __init__
    This just leads to an import loop

--- stylometry/__init__.py
+++ stylometry/__init__.py
@@ -3,7 +3,7 @@
 # TOP-LEVEL MODULES
 ###########################################################
 
-from .extract import *
-from .download import *
-from .classify import *
-from .cluster import *
+#from .extract import *
+#from .download import *
+#from .classify import *
+#from .cluster import *

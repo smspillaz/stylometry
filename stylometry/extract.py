3037c2d77fa1152cabdf1b2b87df547c424904a8
Sam Spilsbury <smspillaz@gmail.com>
2019-01-09 08:26:58 +0200

    extract: Read binary, then decode

--- stylometry/extract.py
+++ stylometry/extract.py
@@ -13,7 +13,7 @@ DEFAULT_AUTHOR = "Unknown"
 class StyloDocument(object):
 
     def __init__(self, file_name, author=DEFAULT_AUTHOR):
-        self.doc = open(file_name, "r").read().decode(encoding='utf-8', errors='ignore')
+        self.doc = open(file_name, "rb").read().decode(encoding='utf-8', errors='ignore')
         self.author = author
         self.file_name = file_name
         self.tokens = word_tokenize(self.doc)
